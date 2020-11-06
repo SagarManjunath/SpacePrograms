60fc2cfa42527430281fa9b75824b9d56ae6c56c
SagarManjunath <sagar.m@siemens.com>
2020-11-06 16:23:41 +0530

    step

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { HomeService } from '../home.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  step:any;
   year:number;
   SuccessLaunch:boolean=false;
   Successland:boolean=false;
